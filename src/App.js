ede384daa9a23edb239dcf075353744104b49f8d
DOOJ SAHU <doojsahu@DOOJs-MacBook-Pro.local>
2024-04-30 08:32:26 +0530

    front end react initially setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        ITS FrontEnd
+      </header>
+    </div>
+  );
+}
+
+export default App;
